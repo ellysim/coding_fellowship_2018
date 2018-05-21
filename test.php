5e3fb2c478052994c28e4acee2cb0a99c443eb5f
Elly Sim <elly.sim@lessannoyingcrm.com>
2018-05-21 14:01:53 -0500

    First Blog Change
    Before changing format of blog

---
+++ test.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+      <title>PHP Practice</title>
+  </head>
+
+  <body>
+
+<?php
+  $name=Elly;
+  $age=20;
+  Echo "My name is $name and I am $age years old <br>";
+
+
+
+// define function
+  function getSum ($num1, $num2){
+    $sum = $num1 + $num2;
+    echo "Sum of the two numbers $num1 and $num2 is : $sum";
+  }
+// call function
+  getSum(8,15);
+  ?>
+
+  </body>
+</html>
